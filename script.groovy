41ac14d17411986b7f24a19ddb505eda176689ce
mustafafarag <mustafafarag23@gmail.com>
2022-09-22 15:21:29 +0200

    Add initial project

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t ahmedfarag9/my-repo:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push ahmedfarag9/my-repo:jma-2.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
